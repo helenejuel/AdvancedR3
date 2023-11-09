3b7580f6af0504d3effc6d442155736c79be31b9
Helene Baek Juel <helene.juel@gmail.com>
2023-11-09 11:10:00 +0100

    Added ggplot package to description, function to draw graph to functions.r, and adding graph function to the targets pipeline

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,18 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+#' Distribution plot
+#'
+#' @param data lipidomics data
+#'
+#' @return A facetwrapped ggplot
+
+plot_distributions <- function(data) {
+    ggplot2::ggplot(data,
+                    ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(dplyr::vars(metabolite), scales = "free")
+
+}
+
